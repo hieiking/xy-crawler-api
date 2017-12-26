cc9e8837b4d3d1cc23cd4ae5166765684ca47576
hieiking <hieikingx@gmail.com>
2017-12-27 01:19:05 +0800

    Add docs route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+	get '/docs/index', to: 'docs#index'
+
 	namespace :api do
 	  namespace :v1 do
 	    resources :products, only: [:index, :create, :show, :update, :destroy]
@@ -9,4 +11,5 @@ Rails.application.routes.draw do
 	    # resources :microposts, only: [:index, :create, :show, :update, :destroy]
 	  end
 	end
+
 end
